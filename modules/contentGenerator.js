ebd76abd84bffc6038002ab5358794e8222b831f
Joris Lamers <14138433@student.hhs.nl>
2019-02-11 19:07:17 +0100

    fsdfsf
    Took 9 minutes

--- modules/contentGenerator.js
+++ modules/contentGenerator.js
@@ -2,12 +2,12 @@ let lorem = require('fast-lorem-ipsum');
 
 function ContentGenerator() {
 
-    function dispatch(type, seed, acc) {
-        switch(type) {
+    function dispatch(type, counters) {
+        switch (type) {
             case 'string':
                 return lorem(10, 101);
             case 'id':
-                return acc++;
+                return counters.acc++;
             case 'image':
                 return 'https://picsum.photos/200/300?random';
         }
@@ -16,11 +16,13 @@ function ContentGenerator() {
     this.generate = function (profile) {
 
         let mockObject = {};
-        let acc = 0;
-        let seed = 1234;
+        let counters = {
+            acc: 0,
+            seed: 1234
+        };
 
         profile.content.forEach(function (field) {
-            mockObject[field.name] = dispatch(field.type, seed, acc);
+            mockObject[field.name] = dispatch(field.type, counters);
         });
 
         return mockObject;
